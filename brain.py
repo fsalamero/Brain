2835264d2958372b76402c917d4b33cd3633c46f
Fernando Salamero <fsalamero@gmail.com>
2013-03-29 19:53:41 +0100

    Añadida definición previa de etiqueta y función que genera un punto al azar fuera de la pantalla

--- brain.py
+++ brain.py
@@ -2,6 +2,7 @@
 # -*- coding:utf-8 -*-
 
 import pilas
+import random
 
 ALTO = 600
 ANCHO = 800
@@ -9,6 +10,23 @@ ANCHO = 800
 fullscreen = True
 
 pilas.iniciar(ancho=ANCHO, alto=ALTO, pantalla_completa=True)
+
+
+class Etiqueta(pilas.actores.Actor):
+
+    def __init__(self, texto):
+        x, y = generar_posicion_offscreen()
+        pilas.actores.Actor.__init__(self, x=x, y=y)
+        ancho, alto = pilas.mundo.motor.obtener_area_de_texto(texto, 14)
+        aspecto = pilas.imagenes.cargar_superficie(ancho=ancho + 15,
+                                                    alto=alto + 5)
+        aspecto.pintar(pilas.colores.azul)
+        aspecto.texto(texto, x=5, y=15, magnitud=14,
+                        color=pilas.colores.blanco)
+        self.imagen = aspecto
+        self.aprender(pilas.habilidades.Arrastrable)
+        self.x = [0], 0.5
+        self.y = [0], 0.5
 
 
 class Item(object):
@@ -30,7 +48,7 @@ class Item(object):
         self.imagen = imagen
 
 
-def salir(evento):
+def gestionar_tecla(evento):
     global fullscreen
     if evento.codigo == pilas.simbolos.q:
         pilas.terminar()
@@ -42,8 +60,19 @@ def salir(evento):
             pilas.atajos.modo_pantalla_completa()
             fullscreen = True
 
-pilas.eventos.pulsa_tecla.conectar(salir)
+
+def generar_posicion_offscreen():
+    '''Genera un punto al azar fuera de la ventana'''
+    x = random.randint(-ANCHO, ANCHO)
+    if abs(x) <= ANCHO / 2:
+        y = random.choice([ALTO, -ALTO])
+    else:
+        y = random.randint(-ALTO, ALTO)
+    return x, y
+
+pilas.eventos.pulsa_tecla.conectar(gestionar_tecla)
 
 idea = Item()
+etiqueta = Etiqueta(u"1ESA")
 
 pilas.ejecutar()
